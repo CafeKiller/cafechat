44f9af263404ed3949762d459471bbaaf7823815
CoffeeKiller <18320210361@163.com>
2023-09-13 21:52:25 +0800

    update 2023年9月13日21:52:21

--- src/main/java/com/cafe/fx/wx/view/register/RegisterForm.java
+++ src/main/java/com/cafe/fx/wx/view/register/RegisterForm.java
@@ -1,7 +1,10 @@
 package com.cafe.fx.wx.view.register;
 
 import com.cafe.fx.wx.vo.RegisterVo;
+import com.dlsc.formsfx.model.structure.Field;
 import com.dlsc.formsfx.model.structure.Form;
+import com.dlsc.formsfx.model.structure.Group;
+import com.dlsc.formsfx.model.structure.PasswordField;
 import com.dlsc.formsfx.view.renderer.FormRenderer;
 
 public class RegisterForm {
@@ -26,6 +29,30 @@ public class RegisterForm {
         String nickname = "昵称";
         String username = "账号";
         String password = "密码";
+        String placeholderTextFmt = "请输入 %s";
+        String requiredTextFmt = "%s 必填";
+
+        /* 初始化表单对象 */
+        form = Form.of(
+                Group.of(
+                        Field.ofStringType(vo.nicknameProperty())
+                                .label(nickname)
+                                .placeholder(String.format(placeholderTextFmt, nickname))
+                                .required(String.format(requiredTextFmt, nickname)),
+
+                        Field.ofStringType(vo.usernameProperty())
+                                .label(username)
+                                .placeholder(String.format(placeholderTextFmt, username))
+                                .required(String.format(requiredTextFmt, username)),
+
+                        PasswordField.ofPasswordType(vo.passwordProperty())
+                                .label(password)
+                                .placeholder(String.format(placeholderTextFmt, password))
+                                .required(String.format(requiredTextFmt, password))
+                )
+        );
+        /* 渲染表单对象 */
+        renderer = new FormRenderer(form);
     }
 
     public RegisterVo getVo() {
