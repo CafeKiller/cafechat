50cd0c748c7dc5b9416ddf3a98520455dd9291ba
CoffeeKiller <18320210361@163.com>
2023-09-13 22:35:47 +0800

    update 2023年9月13日22:35:43

--- src/main/java/com/cafe/fx/wx/core/FXContext.java
+++ src/main/java/com/cafe/fx/wx/core/FXContext.java
@@ -1,10 +1,40 @@
 package com.cafe.fx.wx.core;
 
+import javafx.stage.Stage;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class FXContext {
 
     private static final Map<String, Object> cache = new HashMap<>();
+
+    private static <T> T get(String key){
+        return (T) cache.get(key);
+    }
+    private static void put(String key, Object value){
+        cache.put(key, value);
+    }
+
+    interface K {
+        String PRIMARY_STAGE = "PRIMARY_STAGE";
+        String LOGIN_STAGE = "LOGIN_STAGE";
+    }
+
+    public static Stage getPrimaryStage(){
+        return get(K.PRIMARY_STAGE);
+    }
+
+    public static void setPrimaryStage(String primaryStage){
+        put(K.PRIMARY_STAGE, primaryStage);
+    }
+
+    public static Stage getLoginStage(){
+        return get(K.LOGIN_STAGE);
+    }
+
+    public static void setLoginStage(){
+//        put(K.LOGIN_STAGE, );
+    }
 
 }
