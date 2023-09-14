4a685ed112d9969253efbd36ad90f519ed452404
CoffeeKiller <18320210361@163.com>
2023-09-14 23:17:54 +0800

    update LoginStage

--- src/main/java/com/cafe/fx/wx/view/login/LoginStage.java
+++ src/main/java/com/cafe/fx/wx/view/login/LoginStage.java
@@ -1,5 +1,7 @@
 package com.cafe.fx.wx.view.login;
 
+import com.cafe.fx.wx.core.FXContext;
+import com.cafe.fx.wx.core.FXIcon;
 import javafx.stage.Stage;
 
 public class LoginStage extends Stage {
@@ -11,6 +13,13 @@ public class LoginStage extends Stage {
     public static LoginStage build(){
         LoginStage stage = new LoginStage();
         stage.setTitle("CAFE Chat bate");
+
+        /* 设置Login页面参数 */
+        stage.getIcons().clear();
+        stage.getIcons().add(FXIcon.logo());
+        stage.setWidth(450);
+        stage.setHeight(350);
+        stage.initOwner(FXContext.getPrimaryStage());
 
         return stage;
     }
