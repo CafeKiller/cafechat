facdfc080c0c84d6d076535163de598eef53efab
CoffeeKiller <18320210361@163.com>
2023-09-14 23:04:24 +0800

    add LoginController

---
+++ src/main/java/com/cafe/fx/wx/view/login/LoginController.java
@@ -0,0 +1,27 @@
+package com.cafe.fx.wx.view.login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+/* 登录页面控制器 */
+public class LoginController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void onRegisterClick(ActionEvent actionEvent){
+
+    }
+
+    public void onLoginClick(ActionEvent actionEvent){
+
+    }
+
+}
