3870438e9a47a2564b1ea338e80291c21eab5d45
CoffeeKiller <18320210361@163.com>
2023-09-16 00:51:58 +0800

    update 2023年9月16日00:51:54

---
+++ src/main/java/com/cafe/fx/wx/view/chat/ChatRowController.java
@@ -0,0 +1,8 @@
+package com.cafe.fx.wx.view.chat;
+
+import javafx.scene.control.ListCell;
+
+public class ChatRowController extends ListCell {
+
+
+}
---
+++ src/main/java/com/cafe/fx/wx/vo/MessageVO.java
@@ -0,0 +1,6 @@
+package com.cafe.fx.wx.vo;
+
+public class MessageVO {
+
+
+}
