fe357db56628ee7d357e39f25b8a1ec13fb2a028
CoffeeKiller <18320210361@163.com>
2023-09-15 23:17:29 +0800

    add MainController

---
+++ src/main/java/com/cafe/fx/wx/view/main/MainController.java
@@ -0,0 +1,17 @@
+package com.cafe.fx.wx.view.main;
+
+public class MainController {
+
+    public void onChatClick(){
+
+    }
+    public void onContactsClick(){
+
+    }
+    public void onMinimizeClick(){
+
+    }
+    public void onExitClick(){
+
+    }
+}
