bf67313989091d5ee95f588594150bfd5a139fdf
CoffeeKiller <18320210361@163.com>
2023-09-15 23:22:18 +0800

    update 2023年9月15日23:22:15

--- src/main/java/com/cafe/fx/wx/view/main/MainController.java
+++ src/main/java/com/cafe/fx/wx/view/main/MainController.java
@@ -1,8 +1,40 @@
 package com.cafe.fx.wx.view.main;
 
-public class MainController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import javax.swing.text.html.ImageView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    public ImageView avatarImageView;
+    public ToggleButton chatButton;
+    public ToggleButton contactsButton;
+    public VBox listVBox;
+    public Pane mainPane;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeUI();
+        initializeEvent();
+    }
+
+    void initializeUI(){
+
+    }
+
+    void initializeEvent(){
+
+    }
 
     public void onChatClick(){
+        listVBox.getChildren().clear();
 
     }
     public void onContactsClick(){
