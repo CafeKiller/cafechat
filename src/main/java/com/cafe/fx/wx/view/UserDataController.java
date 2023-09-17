d23a523a059d927f73921edefbcfc37dafecc79b
CoffeeKiller <18320210361@163.com>
2023-09-17 22:41:50 +0800

    add ContactsProfileController

---
+++ src/main/java/com/cafe/fx/wx/view/UserDataController.java
@@ -0,0 +1,7 @@
+package com.cafe.fx.wx.view;
+
+public interface UserDataController {
+
+    void initialize(Object data);
+
+}
---
+++ src/main/java/com/cafe/fx/wx/view/contacts/ContactsProfileController.java
@@ -0,0 +1,11 @@
+package com.cafe.fx.wx.view.contacts;
+
+import com.cafe.fx.wx.view.UserDataController;
+
+public class ContactsProfileController implements UserDataController {
+
+    @Override
+    public void initialize(Object data) {
+
+    }
+}
