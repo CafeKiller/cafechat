455e82d93c4d77c820f8ff3470ae7bba91e47a15
CoffeeKiller <18320210361@163.com>
2023-09-17 20:55:19 +0800

    add ContactsListController and ContactsRowController

---
+++ src/main/java/com/cafe/fx/wx/view/contacts/ContactsListController.java
@@ -0,0 +1,20 @@
+package com.cafe.fx.wx.view.contacts;
+
+import javafx.fxml.Initializable;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ContactsListController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void onApplyClick(ActionEvent actionEvent){
+
+    }
+}
---
+++ src/main/java/com/cafe/fx/wx/view/contacts/ContactsRowController.java
@@ -0,0 +1,10 @@
+package com.cafe.fx.wx.view.contacts;
+
+import com.cafe.fx.wx.vo.ContactsVO;
+import javafx.scene.control.ListCell;
+
+public class ContactsRowController extends ListCell<ContactsVO> {
+
+
+
+}
