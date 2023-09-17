9a105af191a90551d37ba2c9f0acb96adecc6825
CoffeeKiller <18320210361@163.com>
2023-09-17 21:06:13 +0800

    update 2023年9月17日21:06:09

--- src/main/java/com/cafe/fx/wx/view/contacts/ContactsListController.java
+++ src/main/java/com/cafe/fx/wx/view/contacts/ContactsListController.java
@@ -1,17 +1,42 @@
 package com.cafe.fx.wx.view.contacts;
 
+import com.cafe.fx.wx.vo.ContactsVO;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 
-import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ContactsListController implements Initializable {
 
+    public ListView<ContactsVO> contactsVOListView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initializeUI();
+        initializeEvent();
+        renderDebugDate();
+    }
 
+    void initializeUI(){
+        contactsVOListView.setCellFactory(f -> new ContactsRowController());
+    }
+
+    void initializeEvent(){
+
+    }
+
+    void renderDebugDate(){
+        ContactsVO c1 = new ContactsVO();
+        c1.setId(233L);
+        c1.setNickname("ATX-wuying");
+        contactsVOListView.getItems().add(c1);
+
+        ContactsVO c2 = new ContactsVO();
+        c2.setId(322L);
+        c2.setNickname("Coffee_Killer");
+        contactsVOListView.getItems().add(c2);
     }
 
     public void onApplyClick(ActionEvent actionEvent){
--- src/main/java/com/cafe/fx/wx/view/contacts/ContactsRowController.java
+++ src/main/java/com/cafe/fx/wx/view/contacts/ContactsRowController.java
@@ -1,10 +1,43 @@
 package com.cafe.fx.wx.view.contacts;
 
+import com.cafe.fx.wx.core.FXAvatar;
 import com.cafe.fx.wx.vo.ContactsVO;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 public class ContactsRowController extends ListCell<ContactsVO> {
 
+    public HBox contactsRowHBox;
+    public ImageView avatarImageView;
+    public Label nicknameLabel;
 
+    private FXMLLoader loader;
 
+    @Override
+    protected void updateItem(ContactsVO item, boolean empty) {
+        super.updateItem(item, empty);
+        setText(null);
+
+        if (empty || item == null){
+            setGraphic(null);
+            return;
+        }
+
+        if (loader == null){
+            loader = new FXMLLoader(getClass().getResource("/views/"+getClass().getSimpleName()+".fxml"));
+            loader.setController(this);
+            try {
+                loader.load();
+            }catch (Exception e){
+                throw new RuntimeException(e);
+            }
+        }
+        avatarImageView.setImage(FXAvatar.load(item.getAvatar()));
+        nicknameLabel.setText(item.getNickname());
+
+        setGraphic(contactsRowHBox);
+    }
 }
