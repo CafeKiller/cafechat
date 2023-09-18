852d62cd323ef1c78b11843c561c8410f50f78bf
CoffeeKiller <18320210361@163.com>
2023-09-18 21:17:36 +0800

    update 2023年9月18日21:17:33

--- src/main/java/com/cafe/fx/wx/view/contacts/ContactsProfileController.java
+++ src/main/java/com/cafe/fx/wx/view/contacts/ContactsProfileController.java
@@ -1,11 +1,34 @@
 package com.cafe.fx.wx.view.contacts;
 
+import com.cafe.fx.wx.core.FXAvatar;
 import com.cafe.fx.wx.view.UserDataController;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class ContactsProfileController implements UserDataController {
+
+    public ImageView avatarImageView;
+    public Label nicknameLabel;
+
+    private Long contactsId;
+
 
     @Override
     public void initialize(Object data) {
 
+        contactsId = (Long)data;
+        initializeUI();
+        initializeEvent();
+        renderDebugDate();
+
     }
+    void initializeUI(){}
+    void initializeEvent(){}
+    void renderDebugDate(){
+        nicknameLabel.setText("WxID: " + contactsId);
+        avatarImageView.setImage(FXAvatar.def());
+    }
+
+    public void onSendMessageClick(ActionEvent actionEvent){ }
 }
