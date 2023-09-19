0026a71fa72deeef5b61b49a2f0642d7883c3e77
CoffeeKiller <18320210361@163.com>
2023-09-19 23:30:52 +0800

    update 2023年9月19日23:30:49

--- src/main/java/com/cafe/fx/wx/view/chat/ChatMainController.java
+++ src/main/java/com/cafe/fx/wx/view/chat/ChatMainController.java
@@ -1,10 +1,15 @@
 package com.cafe.fx.wx.view.chat;
 
+import com.cafe.fx.wx.html.Html;
 import com.cafe.fx.wx.view.UserDataController;
+import com.cafe.fx.wx.vo.MessageVO;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
+
+import java.util.Date;
 
 public class ChatMainController implements UserDataController {
 
@@ -12,11 +17,15 @@ public class ChatMainController implements UserDataController {
     public WebView chatWebView;
     public TextArea messageTextArea;
 
+    private JSObject js;
     private Long contactsId;
+
 
     @Override
     public void initialize(Object data) {
-
+        contactsId = (Long)data;
+        initializeUI();
+        initializeEvent();
     }
 
     void initializeUI(){
@@ -27,15 +36,35 @@ public class ChatMainController implements UserDataController {
     }
     void renderDebugDate(){
         nicknameLabel.setText("WxID: " + contactsId);
+        MessageVO from = new MessageVO();
+        from.setNickname("ATX_wuying");
+        from.setMessage("HELLO 我是ATX_wuying");
+        from.setTimestamp(new Date().getTime());
+        append(from, true);
     }
-    public void onClearClick(ActionEvent actionEvent){
 
+    void append(MessageVO vo, boolean isContacts){
+        js.call("append", Html.of(vo.getNickname(), vo.getTimestamp(), vo.getMessage(), isContacts));
+    }
+
+    public void onClearClick(ActionEvent actionEvent){
+        js.call("clear");
     }
     public void onCloseClick(ActionEvent actionEvent){
 
     }
     public void onSendClick(ActionEvent actionEvent){
+        String content = messageTextArea.getText();
+        if (content.trim().length() == 0){
+            return;
+        }
 
+        MessageVO from = new MessageVO();
+        from.setNickname("My");
+        from.setMessage(content);
+        from.setTimestamp(new Date().getTime());
+        append(from, false);
+        messageTextArea.clear();
     }
 
 
