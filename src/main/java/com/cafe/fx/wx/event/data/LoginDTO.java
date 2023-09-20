0a409d3f7bc1fad230553f3d64852b013740bb46
CoffeeKiller <18320210361@163.com>
2023-09-20 23:02:39 +0800

    add DTO

---
+++ src/main/java/com/cafe/fx/wx/event/data/LoginDTO.java
@@ -0,0 +1,24 @@
+package com.cafe.fx.wx.event.data;
+
+public class LoginDTO {
+
+    private Long id;
+
+    private String token;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ src/main/java/com/cafe/fx/wx/event/data/UserinfoDTO.java
@@ -0,0 +1,33 @@
+package com.cafe.fx.wx.event.data;
+
+public class UserinfoDTO {
+
+    private String nickname;
+
+    private String username;
+    private String avatar;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
