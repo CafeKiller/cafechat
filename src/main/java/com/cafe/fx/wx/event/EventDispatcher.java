110e020bfa61dbcbaa0849f55de07e3fd9efd380
CoffeeKiller <18320210361@163.com>
2023-09-20 22:56:49 +0800

    update EventDispatcher

--- src/main/java/com/cafe/fx/wx/event/EventDispatcher.java
+++ src/main/java/com/cafe/fx/wx/event/EventDispatcher.java
@@ -1,9 +1,13 @@
 package com.cafe.fx.wx.event;
 
+import com.cafe.fx.wx.constant.IMCode;
+import com.cafe.fx.wx.core.FX;
 import com.cafe.fx.wx.core.FXContext;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
@@ -19,9 +23,27 @@ public class EventDispatcher {
         register(event, dataConsumer, FXContext.getPrimaryStage());
     }
 
-    public static <T> void register(Event event, Consumer<T> dataConsumer, Stage stage){
+    public static <T> void register(Event event, Consumer<T> dataConsumer, Stage owner){
         callers.put(event, (code, data, message) -> {
-            if (code != )
+            if (code != IMCode.OK){
+                Platform.runLater(() -> FX.error(code + ":" + message, owner));
+            } else {
+                dataConsumer.accept((T) data);
+            }
         });
     }
+
+    public static <T> void dispatch(Event event, T data){
+        dispatch(event, IMCode.OK, data, null);
+    }
+
+    public static <T> void dispatch(Event event, int code, String message){
+        dispatch(event, code , null, message);
+    }
+
+    public static <T> void dispatch(Event event, int code, T data, String message){
+        EventCaller<?> caller = callers.get(event);
+        Optional.ofNullable(caller).ifPresent(c -> ((EventCaller<T>) c).accept(code, data, message));
+    }
+
 }
